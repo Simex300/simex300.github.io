3062b97453785ff02d6e034f249c7f3d5b181ad8
Adonis Almonte <adonis0444@gmail.com>
2021-08-21 19:41:56 -0400

    Re-factoring the website on desktop and mobile mode, add extra events

--- js/index.js
+++ js/index.js
@@ -1,20 +1,52 @@
-document.querySelectorAll(".grid-item").forEach(e => {{
-    e.addEventListener("touchend", openSkills);
-}});
+window.addEventListener('load', removeDOMClasses)
+window.addEventListener('resize', removeDOMClasses)
 
-document.querySelectorAll(".flex-item").forEach(e => {{
-    e.addEventListener("touchend", openPortfolio);
-}});
+document.querySelectorAll(".grid-item").forEach(e => {
+    if (!navigator.userAgentData.mobile) 
+        e.addEventListener("click", openSkills);
+    else
+        e.addEventListener("touchend", openSkills);
+});
+
+document.querySelectorAll(".flex-item").forEach(e => {
+    if (navigator.userAgentData.mobile)
+        e.addEventListener("touchend", openPortfolio);
+});
 
 function openSkills() {
-    this.children[1].classList.toggle("active")
+    const hadActive = this.children[1].classList.contains("active")
+    document.querySelectorAll(".grid-item .menu").forEach(e => {
+        e.classList.remove("active");
+    });
+    if(!hadActive)
+        this.children[1].classList.toggle("active")
 }
 
 function openPortfolio() {
     const hadActive = this.classList.contains("active")
-    document.querySelectorAll(".flex-item").forEach(e => {{
+    document.querySelectorAll(".flex-item").forEach(e => {
         e.classList.remove("active");
-    }});
+    });
     if(!hadActive)
         this.classList.toggle("active")
+}
+
+function removeDOMClasses() {
+    if(window.innerWidth <= 800) {
+        // Tablet and mobile changes
+        document.querySelectorAll(".menu .menu-item .squarebox .alt").forEach(squarebox => {
+            if(squarebox.classList.contains("alt"))
+                squarebox.classList.remove("alt")
+        })
+    }
+    else {
+        // Tablet and mobile changes
+        document.querySelectorAll(".menu .menu-item .squarebox").forEach(squarebox => {
+            console.log(squarebox.children);
+            for (const child in squarebox.children) {
+                if(!child.classList.contains("alt"))
+                child.classList.add("alt")
+            }
+        })
+    }
 }
